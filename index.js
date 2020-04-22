f82886577451b8edc0435d49cf1591f6df812453
Aldo AC <aldo.castillo.13@gmail.com>
2020-04-22 15:09:13 -0300

    Add changes index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require ('express');
+const application = express();
+
+const apiInfo = {
+    name: "First Express Api",
+    version: "1.0.0",
+    author: "aldo_mec_acc16",
+    contactInfo:{
+        email: "aldo.castillo.13@gmail.com",
+        wsp: "261-6934658"
+    }
+}
+
+application.get('/name', (request, response) => {
+    const name = {name: apiInfo.name};
+    response.send(name);
+});
+
+application.get('/version', (request, response) => {
+    const version = {version: apiInfo.version};
+    response.send(version);
+});
+
+application.get('/author', (request, response) => {
+    const author = {author: apiInfo.author};
+    response.send(author);
+});
+
+application.get('/contact', (request, response) => {
+    const contact = {contact: apiInfo.contactInfo};
+    response.send(contact);
+});
+
+application.get('/', (request, response) => {
+    response.send('Respuesta del servidor');
+});
+
+application.get('/info-api', (request, response) => {
+    response.send(apiInfo);
+});
+
+
+application.listen('3000', () => {
+    console.log('Server listening at port 3000');
+});
